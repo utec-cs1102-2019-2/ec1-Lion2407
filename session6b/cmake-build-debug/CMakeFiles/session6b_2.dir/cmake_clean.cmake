9a45d042486ab881766cbaa07c1479915a85f42f
Leonardo Torres <leonardo.torres.t@utec.edu.pe>
2019-09-26 11:05:18 -0500

    session6b

---
+++ session6b/cmake-build-debug/CMakeFiles/session6b_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session6b_2.dir/ejercicio2.cpp.obj"
+  "session6b_2.pdb"
+  "session6b_2.exe"
+  "session6b_2.exe.manifest"
+  "libsession6b_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session6b_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
