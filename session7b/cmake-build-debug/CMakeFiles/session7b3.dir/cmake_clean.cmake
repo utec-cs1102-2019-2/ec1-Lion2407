0331eaf04fda416c0f5defe7d099762a904a5a72
Leonardo Torres <leonardo.torres.t@utec.edu.pe>
2019-10-03 11:00:07 -0500

    session7b

---
+++ session7b/cmake-build-debug/CMakeFiles/session7b3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session7b3.dir/ejercicio3.cpp.obj"
+  "session7b3.pdb"
+  "session7b3.exe"
+  "session7b3.exe.manifest"
+  "libsession7b3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session7b3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
