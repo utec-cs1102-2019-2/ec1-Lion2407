add61dba5a020475b5e31867b54415d174d88278
Leonardo Torres <leonardo.torres.t@utec.edu.pe>
2019-11-07 11:16:22 -0500

    session11a

---
+++ session11a/cmake-build-debug/CMakeFiles/session11a_1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session11a_1.dir/main.cpp.obj"
+  "CMakeFiles/session11a_1.dir/Tablero.cpp.obj"
+  "CMakeFiles/session11a_1.dir/Bloque.cpp.obj"
+  "session11a_1.pdb"
+  "session11a_1.exe"
+  "session11a_1.exe.manifest"
+  "libsession11a_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session11a_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ session11a/cmake-build-debug/CMakeFiles/tablero.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tablero.cpp.dir/bloque.cpp.obj"
+  "tablero.cpp.pdb"
+  "tablero.cpp.exe"
+  "tablero.cpp.exe.manifest"
+  "libtablero.cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tablero.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
